16c8b69347dfceafe0a909bd25a4dadbe18cf171
Hala Naz <65819346+HalaNHasan@users.noreply.github.com>
2022-12-22 20:43:59 +0300

    added FilterBox component

--- frontend/src/components/FilterBox.js
+++ frontend/src/components/FilterBox.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import { useDispatch } from "react-redux";
+import { setFilteredCountries } from "../redux/reducers";
 
 const FilterBox = () => {
-  return <div className="">FilterBox Component</div>;
+  const dispatch = useDispatch();
+
+  return (
+    <div className="m-3 d-flex justify-content-center">
+      <Form.Control
+        placeholder="Country..."
+        aria-label="Country..."
+        aria-describedby="basic-addon2"
+        onChange={(e) => {
+          dispatch(setFilteredCountries(e.target.value));
+        }}
+      />
+    </div>
+  );
 };
 
 export default FilterBox;
