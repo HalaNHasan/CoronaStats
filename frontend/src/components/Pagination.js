b137555c841cc22b2a4e22c2d05880a75239d794
Hala Naz <65819346+HalaNHasan@users.noreply.github.com>
2022-12-24 19:31:18 +0300

    added Pagination component

---
+++ frontend/src/components/Pagination.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setNextPage, setPrevPage, resetPagination } from "../redux/reducers";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+
+const Pagination = () => {
+  const dispatch = useDispatch();
+
+  const { firstIndex, lastIndex } = useSelector((state) => {
+    return {
+      firstIndex: state.stats.firstIndex,
+      lastIndex: state.stats.lastIndex,
+    };
+  });
+
+  console.log(firstIndex, lastIndex);
+  useEffect(() => {
+    dispatch(resetPagination());
+  }, []);
+  return (
+    <div
+      className="d-flex justify-content-between align-items-center flex-wrap w-100"
+      style={{ position: "relative" }}
+    >
+      {firstIndex > 0 ? (
+        <BsFillArrowLeftCircleFill
+          style={{
+            position: "absolute",
+            top: "-9vh",
+            left: "0.5vw",
+            fontSize: "2.5rem",
+            paddingLeft: "0.5rem",
+          }}
+          role="button"
+          onClick={async () => {
+            dispatch(setPrevPage());
+          }}
+        />
+      ) : null}
+      {lastIndex < 197 ? (
+        <BsFillArrowRightCircleFill
+          style={{
+            position: "absolute",
+            top: "-10vh",
+            right: "0.5vw",
+            fontSize: "2.5rem",
+            paddingRight: "0.5rem",
+          }}
+          role="button"
+          onClick={async () => {
+            dispatch(setNextPage());
+          }}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default Pagination;
--- frontend/src/pages/CountriesPage.js
+++ frontend/src/pages/CountriesPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import FilterBox from "../components/FilterBox";
 import CountriesList from "../components/CountriesList";
+import Pagination from "../components/Pagination";
 
 const CountriesPage = () => {
   return (
     <>
       <FilterBox />
+      <Pagination />
       <CountriesList />
     </>
   );
